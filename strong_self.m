407f95310e7bc7c422cc3e1f5b472739bed554ec
Roland Gröpmair <roland@mapps.ie>
2015-03-03 14:02:51 +0000

    strong_self

---
+++ strong_self.m
@@ -0,0 +1,10 @@
+// strong self
+// Strong self reference
+//
+// IDECodeSnippetCompletionPrefix: strongSelf
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 717F5611-AC63-43D6-BB9A-4F68F1551103
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+typeof(weakSelf) strongSelf = weakSelf;
