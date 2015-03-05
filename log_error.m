c9216c08c9a509d752324e255075a57a85bb311e
Roland Gröpmair <roland@mapps.ie>
2015-03-05 10:42:02 +0000

    added cocoalumberjack snippets

---
+++ log_error.m
@@ -0,0 +1,11 @@
+// log error
+// log error
+//
+// IDECodeSnippetCompletionPrefix: dde
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 11BB3538-E850-48CA-AAD7-C60C46CFFD52
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 1
+
+DDLogError(@"%s: error=%@", __FUNCTION__, error.localizedDescription);
---
+++ log_warning.m
@@ -0,0 +1,11 @@
+// log warning
+// log warning
+//
+// IDECodeSnippetCompletionPrefix: ddw
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 11BB3538-E850-48CA-AAD7-C60C46CFFD53
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 1
+
+DDLogWarning(@"%s: error=%@", __FUNCTION__, error.localizedDescription);
